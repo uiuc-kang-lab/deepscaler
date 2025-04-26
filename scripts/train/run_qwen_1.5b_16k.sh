99c76f1fabd5303666bb33fc5941df598549e9d0
Yuxuan Zhu <yxx404@illinois.edu>
2025-04-26 00:59:29 +0000

    tweak scripts

--- scripts/train/run_qwen_1.5b_16k.sh
+++ scripts/train/run_qwen_1.5b_16k.sh
@@ -20,22 +20,22 @@ done
 
 # Set default model path if not provided
 if [ -z "$MODEL_PATH" ]; then
-    MODEL_PATH="Qwen/Qwen2.5-1.5B"
+    MODEL_PATH="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
 fi
 
 # Train over a single node, 8 A100-80GB GPUs.
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
-    data.train_files=/workspace/rllm/data/eurus_math_train.parquet \
-    data.val_files=/workspace/rllm/data/eurus_math_validation.parquet \
-    data.train_batch_size=56 \
-    data.val_batch_size=224 \
+    data.train_files=$HOME/rl-mlvr/rllm/data/eurus_math_train.parquet \
+    data.val_files=$HOME/rl-mlvr/rllm/data/aime.parquet \
+    data.train_batch_size=128 \
+    data.val_batch_size=512 \
     data.max_prompt_length=1024 \
     data.max_response_length=8192 \
     actor_rollout_ref.model.path=$MODEL_PATH  \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=28 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=32768 \
     actor_rollout_ref.actor.use_kl_loss=True \
@@ -51,19 +51,19 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.temperature=0.6 \
     actor_rollout_ref.rollout.val_temperature=0.6 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
-    actor_rollout_ref.rollout.n=7 \
-    actor_rollout_ref.rollout.n_val=7 \
+    actor_rollout_ref.rollout.n=8 \
+    actor_rollout_ref.rollout.n_val=8 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name='deepscaler-eurus-math' \
-    trainer.experiment_name='qwen-distill-1.5b-8k' \
+    trainer.experiment_name='qwen-distill-1.5b' \
     +trainer.val_before_train=True \
-    trainer.n_gpus_per_node=7 \
+    trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
-    trainer.save_freq=5 \
-    trainer.test_freq=20 \
+    trainer.save_freq=10 \
+    trainer.test_freq=10 \
     trainer.default_hdfs_dir=null \
-    trainer.default_local_dir=/workspace/checkpoints \
-    trainer.total_epochs=30 "${@:1}"
+    trainer.default_local_dir=$HOME/rl-mlvr/rllm/checkpoints \
+    trainer.total_epochs=1 "${@:1}"
